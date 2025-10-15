e0195f32a4b24cdfd1caca8ae2edb82f9528c36e
mszymkiewicz <malgorzata@aleksander.pro>
2025-10-15 15:18:04 +0200

    access_control1

---
+++ access_control/objects.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE DATABASE poc;
+CREATE OR REPLACE SCHEMA poc;
+CREATE OR REPLACE TABLE poc
+(
+    id int,
+    description text
+);
+
+-- Normally I would differentiate these warehouses with size etc. 
+-- For exaxmple the one for writer would be bigger, but for now I am looking at the cost of the test account. 
+CREATE WAREHOUSE READER_WH WITH 
+WAREHOUSE_SIZE = 'X-SMALL'
+AUTO_SUSPEND = 60
+AUTO_RESUME = TRUE;
+
+CREATE WAREHOUSE WRITER_WH WITH 
+WAREHOUSE_SIZE = 'X-SMALL'
+AUTO_SUSPEND = 60
+AUTO_RESUME = TRUE;
+
+--cleaning up
+DROP WAREHOUSE WRITER_WH;
+DROP WAREHOUSE READER_WH;
+DROP DATABASE poc;
